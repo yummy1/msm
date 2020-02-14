20175591fd6473d57afb4b7758f2032ee1d155bc
yummyMao <1431608414@qq.com>
2020-02-15 00:23:36 +0800

    修改密码完成,接口服务器宕机了,所以之后再测试

--- src/components/AppHeader/index.vue
+++ src/components/AppHeader/index.vue
@@ -6,7 +6,7 @@
         </a>
         <el-dropdown class="el-dropdown" @command="handleCommand">
             <span class="el-dropdown-link">
-                下拉菜单
+                {{user.name}}
                 <i class="el-icon-arrow-down el-icon--right"></i>
             </span>
             <el-dropdown-menu slot="dropdown">
@@ -14,36 +14,137 @@
                 <el-dropdown-item command="b">退出登录</el-dropdown-item>
             </el-dropdown-menu>
         </el-dropdown>
+<!--        修改密码弹窗-->
+        <el-dialog title="修改密码" :visible.sync="dialogFormVisible" width="400px">
+            <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" label-width="100px" style="width: 300px">
+                <el-form-item label="旧密码" prop="oldPass">
+                    <el-input type="password" v-model="ruleForm.oldPass" autocomplete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="新密码" prop="pass">
+                    <el-input type="password" v-model="ruleForm.pass" autocomplete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="确认密码" prop="checkPass">
+                    <el-input type="password" v-model="ruleForm.checkPass" autocomplete="off"></el-input>
+                </el-form-item>
+                <el-form-item>
+                    <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
+                    <el-button @click="resetForm('ruleForm')">重置</el-button>
+                </el-form-item>
+            </el-form>
+        </el-dialog>
     </div>
 </template>
 <script>
 import {logout} from '@/api/login'
+import passwordApi from '@/api/password'
 
     export default {
+      data(){
+        var validatePass = (rule, value, callback) => {
+          if (value === '') {
+            console.log(value)
+            callback(new Error('请输入密码'));
+          } else {
+            passwordApi.checkPassword(this.user.id,value).then(response => {
+              if(response.data.flag){
+                callback();
+              }else{
+                callback(new Error(response.data.message));
+              }
+            })
+          }
+        }
+        var validateCheckPass = (rule, value, callback) => {
+          if (value === '') {
+            callback(new Error('请再次输入密码'));
+          } else if (value !== this.ruleForm.pass) {
+            callback(new Error('两次输入密码不一致!'));
+          } else {
+            callback();
+          }
+        };
+        return {
+          dialogFormVisible: true,
+          user:JSON.parse(localStorage.getItem('msm-user')),
+          ruleForm: {
+            oldPass: '',
+            pass: '',
+            checkPass: ''
+          },
+          rules: {
+            oldPass: [
+              { required: true, message: '请输入旧密码', trigger: 'blur' },
+              { validator: validatePass, trigger: 'blur' }
+            ],
+            pass: [
+              { required: true, message: '请输入新密码', trigger: 'blur' }
+            ],
+            checkPass: [
+              { required: true, message: '请再次输入密码', trigger: 'blur' },
+              { validator: validateCheckPass, trigger: 'blur' }
+            ]
+          }
+        }
+      },
       methods: {
         handleCommand(command){
           switch (command) {
             case 'a':
-            {
-                this.$message('修改密码')
-            }
+              {
+                this.handlePassword()
+              }
               break;
             case 'b':
               {
-                logout().then(response => {
-                  const resp = response.data
-                  console.log(resp)
-                  if (resp.flag){
-                    localStorage.removeItem('msm-user')
-                    localStorage.removeItem('msm-token')
-                    this.$router.push('/login')
-                  }else {
-                    this.$message('退出登录失败')
-                  }
-                })
+                this.handleLogout()
               }
               break;
             }
+        },
+        //退出登录
+        handleLogout(){
+          logout().then(response => {
+            const resp = response.data
+            console.log(resp)
+            if (resp.flag){
+              localStorage.removeItem('msm-user')
+              localStorage.removeItem('msm-token')
+              this.$router.push('/login')
+            }else {
+              this.$message('退出登录失败')
+            }
+          })
+        },
+        //修改密码
+        handlePassword(){
+          this.dialogFormVisible = true
+          this.$nextTick(() => {
+            this.$refs['ruleForm'].resetFields()
+          })
+        },
+        //提交新密码
+        submitForm(FormName){
+          this.$refs[FormName].validate(valid => {
+            if (valid){
+              passwordApi.checkPassword(this.user.id, this.ruleForm.checkPass).then(response => {
+                const resp = response.data
+                this.$message({
+                  message: resp.message,
+                  type: resp.flag ? "success" : "error"
+                })
+                if (resp.flag){
+                  this.dialogFormVisible = false
+                  this.handleLogout()
+                }
+              })
+            }else{
+              return false
+            }
+          })
+        },
+        //重置
+        resetForm(formName){
+            this.$refs[formName].resetFields()
         }
       }
     }
